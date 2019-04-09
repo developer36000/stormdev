2e89c20b935d27bfdbd7904f78320c5c13b76b4d
developer36000 <developer36000@gmail.com>
2019-04-09 16:43:00 +0300

    bug fix 02

--- includes/js/videojs/mov.js
+++ includes/js/videojs/mov.js
@@ -1,16 +1,16 @@
 /*film init*/
 $(document).ready(function() {			
 	/*Playbutton*/
-	var fullvid, fullscreen, fullvid_ob, fullvid_ob_mob;
-	fullvid_ob_mob = '.video_block.active_mob';
-	fullvid_ob = '.full_video_block';
-	var default_v = videojs('default');
+	var fullvid, fullscreen, fullvid_ob, fullvid_ob_mob, def, fullvid_mob;
 	
 	if ($(window).width() > 960 ) {
+		
 		$('.icon-play *').on('click tap', function(event) {
+			fullvid_ob = '.full_video_block';
 			fullvid = videojs('fullvideo');
+			def = videojs('default');
 			$(fullvid_ob).addClass('show_close');
-			videofunc(fullvid_ob, fullvid);
+			videofunc(fullvid_ob, fullvid, def);
 		});
 		
 		/*Check if vidoe is still on fullscreen when windows resize*/
@@ -20,26 +20,28 @@ $(document).ready(function() {
 					if (!fullscreen) {
 						fullscreen = true;
 						fullvid.play();
+						def.currentTime(0);
+						def.pause();
 					}
 				} else {
 					if (fullscreen) {
 						fullscreen = false;
-						fullvid.get(0).pause();
 						fullvid.currentTime(0);
+						fullvid.pause();
 						$(fullvid_ob).removeClass('active');
 						$('.video_close_button').remove();
-						default_v.play();
+						def.play();
 					}
 				}
 			}
 		};
 		
 	} else if ($(window).width() < 960 ) {
-		var fullvid_mob = videojs('show_mobile');
+		
 		$('.icon-play *').on('click tap', function(event) {
-			
+			fullvid_mob = videojs('show_mobile');
+			fullvid_ob_mob = '.video_block.active_mob';
 			$(fullvid_ob_mob).addClass('show_close');
-			//$(this).fadeOut();
 			videofunc_mob(fullvid_ob_mob, fullvid_mob);
 		});
 		/*Check if vidoe is still on fullscreen when windows resize*/
@@ -53,11 +55,10 @@ $(document).ready(function() {
 				} else {
 					if (fullscreen) {
 						fullscreen = false;
-						fullvid_mob.get(0).pause();
-						fullvid_mob.currentTime(0);
 						$(fullvid_ob_mob).removeClass('show_full');
-					//	$(fullvid_ob_mob).removeClass('active');
 						$('.video_close_button').remove();
+						fullvid_mob.currentTime(0);
+						fullvid_mob.pause();
 					}
 				}
 			}
@@ -69,41 +70,44 @@ $(document).ready(function() {
 	
 	
 	
-	function videofunc (fullvid_ob, fullvid) {
+	function videofunc (fullvid_ob, fullvid, def) {
 		$('#page-home .video_block').removeClass('active');
 		$(fullvid_ob).addClass('active');
 		$(fullvid_ob + ' video').attr('data-keepplaying', '');
 		$(fullvid_ob + ' video').attr('controls', '');
 		fullvid.requestFullscreen();
 		fullvid.play();
+		def.currentTime(0);
+		def.pause();
 		fullvid.ready(function(){
 			this.on('ended', function() {
 				this.exitFullscreen();
-				this.get(0).pause();
 				$(fullvid_ob).removeClass('active');
-				//$('#page-home .video_block').addClass('active');
 				$('.video_close_button').remove();
 				$(fullvid_ob).removeClass('show_close');
-				default_v.play();
+				def.play();
+				this.currentTime(0);
+				this.pause();
 			});
 			$('.video_close_button').on("click tap", function() {
 				fullvid.exitFullscreen();
 				//$('#page-home .video_block').addClass('active');
 				$(fullvid_ob).removeClass('active');
 				$(fullvid_ob).removeClass('show_close');
-				$(this).remove();
-				fullvid.get(0).pause();
-				default_v.play();
-				
+				$('.video_close_button').remove();
+				def.play();
+				fullvid.currentTime(0);
+				fullvid.pause();
 			});
 		});
+		var xbutton = '<button class="menu-button  video_close_button"></button>';
+		$(fullvid_ob).find('#fullvideo').append(xbutton);
 		$(document).on('fullscreenchange webkitfullscreenchange mozfullscreenchange MSFullscreenChange', function() {
 			console.log(this);
 			console.log('----');
 			console.log(this['fullscreen']);
 		});
-		var xbutton = '<button class="menu-button  video_close_button"></button>';
-		$(fullvid_ob).find('#fullvideo').append(xbutton);
+		
 	}
 	
 	function videofunc_mob (fullvid_ob_mob, fullvid_mob) {
@@ -116,25 +120,22 @@ $(document).ready(function() {
 		fullvid_mob.ready(function(){
 			this.on('ended', function() {
 				this.exitFullscreen();
-				this.get(0).pause();
 				$(fullvid_ob_mob).removeClass('show_full');
 				$(fullvid_ob_mob + ' video').removeAttr('controls');
 				$('.video_close_button').remove();
 				$(fullvid_ob_mob).removeClass('show_close');
+				this.currentTime(0);
+				this.pause();
 			});
 			$('.video_close_button').on("click tap", function() {
 				fullvid_mob.exitFullscreen();
-				$(fullvid_ob_mob).removeClass('show_close');
-				$(this).remove();
 				$(fullvid_ob_mob).removeClass('show_full');
 				$(fullvid_ob_mob + ' video').removeAttr('controls');
-				fullvid_mob.get(0).pause();
+				$('.video_close_button').remove();
+				$(fullvid_ob_mob).removeClass('show_close');
+				fullvid_mob.currentTime(0);
+				fullvid_mob.pause();
 			});
-		});
-		$(document).on('fullscreenchange webkitfullscreenchange mozfullscreenchange MSFullscreenChange', function() {
-			console.log(this);
-			console.log('----');
-			console.log(this['fullscreen']);
 		});
 		if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
 			console.log('iPhone');
@@ -142,6 +143,12 @@ $(document).ready(function() {
 			var xbutton = '<button class="menu-button  video_close_button"></button>';
 			$(fullvid_ob_mob).find('#show_mobile').append(xbutton);
 		}
+		$(document).on('fullscreenchange webkitfullscreenchange mozfullscreenchange MSFullscreenChange', function() {
+			console.log(this);
+			console.log('----');
+			console.log(this['fullscreen']);
+		});
+		
 		
 	}
 	
