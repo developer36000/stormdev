b909cf584e03602317151778924d5ff107aed7c1
developer36000 <developer36000@gmail.com>
2019-04-10 10:48:46 +0300

    videooo

--- includes/js/videojs/mov.js
+++ includes/js/videojs/mov.js
@@ -114,7 +114,7 @@ $(document).ready(function() {
 		$('#page-home .video_block').removeClass('active');
 		$(fullvid_ob_mob).addClass('show_full');
 		$(fullvid_ob_mob + ' video').attr('data-keepplaying', '');
-		$(fullvid_ob_mob + ' video').attr('controls', '');
+		//$(fullvid_ob_mob + ' video').attr('controls', '');
 		fullvid_mob.requestFullscreen();
 		fullvid_mob.currentTime(0);
 		fullvid_mob.play();
@@ -123,7 +123,7 @@ $(document).ready(function() {
 				this.exitFullscreen();
 				this.pause();
 				$(fullvid_ob_mob).removeClass('show_full');
-				$(fullvid_ob_mob + ' video').removeAttr('controls');
+				//$(fullvid_ob_mob + ' video').removeAttr('controls');
 				$('.video_close_button').remove();
 				$(fullvid_ob_mob).removeClass('show_close');
 			});
@@ -131,7 +131,7 @@ $(document).ready(function() {
 				fullvid_mob.exitFullscreen();
 				fullvid_mob.pause();
 				$(fullvid_ob_mob).removeClass('show_full');
-				$(fullvid_ob_mob + ' video').removeAttr('controls');
+				//$(fullvid_ob_mob + ' video').removeAttr('controls');
 				$('.video_close_button').remove();
 				$(fullvid_ob_mob).removeClass('show_close');
 			});
